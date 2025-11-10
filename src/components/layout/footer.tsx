d389b3a89d4e326bac8e007da2975371590fbe5b
Kevin D. Moreira F. <kevindmfx@gmail.com>
2025-11-10 15:55:16 +0000

    o 2025 puppy assister ainda não está centralizado

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
   return (
     <footer className="border-t border-border/40 py-6 md:py-8">
       <div className="container flex items-center justify-center">
-        <p className="text-center text-sm text-muted-foreground">
+        <p className="text-sm text-muted-foreground">
           &copy; {currentYear} Puppy Assister. Todos os direitos reservados.
         </p>
       </div>
