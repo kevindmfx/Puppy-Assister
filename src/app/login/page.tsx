ac479ec3d70c800c94fad071209d44ea6f45af2a
Kevin D. Moreira F. <kevindmfx@gmail.com>
2025-11-10 18:16:23 +0000

    no login, na parte de cima do login fora da caixa quero que escreva um t

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,10 @@ import { LoginForm } from '@/components/login-form';
 
 export default function LoginPage() {
   return (
-    <div className="container flex min-h-[calc(100vh-140px)] items-center justify-center">
+    <div className="container flex min-h-[calc(100vh-140px)] flex-col items-center justify-center gap-8">
+      <h1 className="text-center font-headline text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
+        ACESSO ANTECIPADO
+      </h1>
       <LoginForm />
     </div>
   );
